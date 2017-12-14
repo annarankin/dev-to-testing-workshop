6bf6c016499111a9d6815d63eb708219315dc742
Anna Rankin <anna@annarankin.com>
2017-12-14 07:21:53 -0500

    Updates compatibilityLevel specs

--- frontend/src/compatibility_calculator/index.test.js
+++ frontend/src/compatibility_calculator/index.test.js
@@ -1,11 +1,19 @@
 import { compatibilityLevel } from './'
 
 describe('compatibilityLevel', () => {
-  it('returns 100 when arrays are identical', () => {
-    const listA = ["A", "B", "C", "D"]
-    const listB = ["A", "B", "C"]
-    const listC = [1, 2, 3]
+  const listA = ["A", "B", "C", "D"]
+  const listB = ["A", "B", "C"]
+  const listC = [1, 2, 3]
 
+  it('returns 100 when arrays are identical', () => {
     expect(compatibilityLevel(listA, listA)).toEqual(100)
+  })
+
+  it('returns a whole-number percentage of overlap when arrays are not identical', () => {
+    expect(compatibilityLevel(listA, listB)).toEqual(75)
+  })
+
+  it('returns 0 when given two arrays with no overlap', () => {
+    expect(compatibilityLevel(listA, listC)).toEqual(0)
   })
 })
